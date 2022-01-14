c7c6fbde425e868cba80c5aeda7a64a3655d7883
Marina Titova <marinatells@mail.ru>
2022-01-14 19:30:07 +0300

    init project

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <a href="about">About</a>
+    </nav>
+    <router-view />
+  </div>
+</template>
